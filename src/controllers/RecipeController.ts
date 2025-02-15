d4bf0f0b729e5416050ad3717f91fdd3d69235c5
tiago <tiago.dscouto@hotmail.com>
2025-02-14 23:14:32 -0300

    create table Ingredientes

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,6 +1,48 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Recipe } from "../entities/Recipe";
+import { RecipeIngredient } from "../entities/RecipeIngredient";
 
 class RecipeController {
- 
-}
+  private recipeRepository;
+  private recipeIngredientRepository;
 
-export default RecipeController;
+  constructor() {
+    this.recipeRepository = AppDataSource.getRepository(Recipe);
+    this.recipeIngredientRepository =
+      AppDataSource.getRepository(RecipeIngredient);
+  }
+
+  create = async (req:Request, res:Response) => {
+    try {
+      console.log(req.body);
+      const body = req.body;
+      // validar as informações
+      const recipe = await this.recipeRepository.save(body);
+
+      // recipe.ingredients.save(body.ingredients)
+
+      body.ingredients.forEach(async (ingredient: { name: string }) => {
+        await this.recipeIngredientRepository.save({
+          ...ingredient,
+          recipe_id: recipe.id,
+        });
+      });
+
+      res.status(201).json(recipe);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+
+  getAll = async (req:Request, res:Response) => {
+    try {
+      const recipes = await this.recipeRepository.find({relations: ["ingredients"]});
+      res.json(recipes);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+}
+export default RecipeController
